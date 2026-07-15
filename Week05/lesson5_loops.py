a55b2e8c297ad231e4f8451b705a4b78b46555fa
Prabjap Tuteja <tutejaprabjap@gmail.com>
2026-07-14 20:24:46 -0400

    Complete Lesson 5: Advanced Loops

--- Week05/lesson5_loops.py
+++ Week05/lesson5_loops.py
@@ -1,12 +1,16 @@
 #Function
 def process_sales(sales):
     total = 0
+
     for sale in sales:
         if sale == 0:
             continue
-        total = total + sale
+
+        total += sale
         print(f"Processed Sale: ${sale:.2f}")
+
     print(f"Total Sales: ${total:.2f}")
+
 
 #Main Program
 daily_sales = [125.50, 0, 89.75, 210.00, 0, 450.25]
